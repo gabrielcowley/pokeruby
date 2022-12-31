92fd592268e7f407695e75da1c78edce03b562de
Gabriel Cowley <gabrielcowley@yahoo.co.uk>
2022-12-31 18:18:57 +0000

    Edit level up learnset script
    To take full levels into account

--- util-scripts/add_move_to_level_up_learnsets.py
+++ util-scripts/add_move_to_level_up_learnsets.py
@@ -49,29 +49,43 @@ def get_line_to_insert(pokemon: str, level: int, move: str, sample_with_leading_
     level_space = ' ' if len(level_str) < 2 else ''
     return f'{leading_whitespace}LEVEL_UP_MOVE({level_space}{level_str}, {move}),\n'
 
+def is_level_at_insertion_already_full(learnset_lines: list[str], insertion_idx: int, level_to_insert: int) -> bool:
+    pattern = rf'\s*?LEVEL_UP_MOVE\( ?{str(level_to_insert)},.*?\),\s*?'
+    line_1_match = re.fullmatch(pattern, learnset_lines[insertion_idx])
+    line_2_match = re.fullmatch(pattern, learnset_lines[insertion_idx + 1])
+    line_3_match = re.fullmatch(pattern, learnset_lines[insertion_idx + 2])
+    line_4_match = re.fullmatch(pattern, learnset_lines[insertion_idx + 3])
+    return not (line_1_match is None or line_2_match is None or line_3_match is None or line_4_match is None)
+
 input_lines = read_input()
 move = input_lines[0].strip()
 
 learnset_lines = read_level_up_learnsets()
 
 move_insertions = {}
+already_full_levels = []
 
 for input_line_idx in range(1, len(input_lines)):
     [pokemon, level_str] = [x.strip() for x in input_lines[input_line_idx].split(',')]
     level_for_pokemon = int(level_str)
     index_of_move_insertion = get_index_of_move_insertion(learnset_lines, pokemon, level_for_pokemon)
+    if is_level_at_insertion_already_full(learnset_lines, index_of_move_insertion, level_for_pokemon):
+        already_full_levels.append(f'{pokemon}: level {level_str} is already full.')
     move_insertions[index_of_move_insertion] = (pokemon, level_for_pokemon)
 
-move_insertions_it = iter(sorted(move_insertions.items(), reverse=True))
+if len(already_full_levels) > 0:
+    print('\n'.join(already_full_levels))
+else:
+    move_insertions_it = iter(sorted(move_insertions.items(), reverse=True))
 
-while True:
-    move_insertion = next(move_insertions_it, None)
-    if move_insertion is None:
-        break
-    (move_insertion_idx, (pokemon, level)) = move_insertion
-    line_to_insert = get_line_to_insert(pokemon, level, move, learnset_lines[move_insertion_idx])
-    learnset_lines = learnset_lines[:move_insertion_idx] + [line_to_insert] + learnset_lines[move_insertion_idx:]
+    while True:
+        move_insertion = next(move_insertions_it, None)
+        if move_insertion is None:
+            break
+        (move_insertion_idx, (pokemon, level)) = move_insertion
+        line_to_insert = get_line_to_insert(pokemon, level, move, learnset_lines[move_insertion_idx])
+        learnset_lines = learnset_lines[:move_insertion_idx] + [line_to_insert] + learnset_lines[move_insertion_idx:]
 
-overwrite_file(level_up_learnsets_path, ''.join(learnset_lines))
+    overwrite_file(level_up_learnsets_path, ''.join(learnset_lines))
 
-print('Success!')
+    print('Success!')
